6c3e641babe16454aaf5ba22033d49834a5c20ed
Nitish Raina <nitishraina@gmail.com>
2025-11-12 15:31:48 +0530

    issue fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import MainContent from './components/MainContent';
 import logo from './assets/images/logo.png';
 
 function App() {
-  retun (
+  return (
     <>
       <header>
         <div id="logo-img">
